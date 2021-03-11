176c5c8d0bcf4ab790300cc442ea61356fdaf352
Radiyah <rsabur58@gmail.com>
2021-03-11 09:37:13 -0500

    commit tree 9aa7af4db3e8ed916687c35903a19e4e31d8176f

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+require 'pry'
+
+
+def collect_multiples(limit)
+    # binding.pry
+    (3...limit).find_all{|n| n%3== 0 || n%5 == 0}
+    
+end
+
+def sum_multiples(limit)
+    (3...limit).find_all{|n| n%3== 0 || n%5 == 0}.inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (3...limit).find_all{|n| n%3== 0 || n%5 == 0}
+    end
+
+    def sum_multiples
+        (3...limit).find_all{|n| n%3== 0 || n%5 == 0}.inject(:+)
+    end
+end
